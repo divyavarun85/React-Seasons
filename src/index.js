84f5b05d4d39ad12c0772eb8ac70eebc8f02bf84
divyavarun85 <divyavarun85@gmail.com>
2021-04-11 21:59:42 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
+import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+// How this works: -
+
+// when the page loads, construction function executes,then super, the state is null ,then 
+//requests for geolocation this time(waiting time) page renders latitude as null.
+// after geting resuts setState updates latitude value and page renders with updated value.
+
+
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {lat:null,errorMessage : ''};
+
+        window.navigator.geolocation.getCurrentPosition(
+            position =>{
+                this.setState({lat:position.coords.latitude}); //callback function
+            },
+            err =>{
+                this.setState({errorMessage:err.message})
+            }
+        );
+
+    }
+    
+    render(){
+           if((this.state.lat)&& (!this.state.errorMessage)){
+                return <div> lat:{this.state.lat}</div>
+            }
+            if((!this.state.lat) && (this.state.errorMessage)){
+               return <div> Message :{this.state.errorMessage}</div>
+            }
+
+            if((!this.state.lat) && (!this.state.errorMessage)){
+                return <div>Loading..</div>
+            }
+        
+    }
+}
+
+reactDom.render(
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
